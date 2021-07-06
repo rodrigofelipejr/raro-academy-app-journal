b03260cab96d6a58561f4aa14901abb0ae90c914
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-07-06 19:22:55 -0300

    initial commit

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Journal',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
